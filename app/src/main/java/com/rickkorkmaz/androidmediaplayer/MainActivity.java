40baaf03cae456c46698521a5f52920b0349dc73
Rick <rickkorkmaz@gmail.com>
2020-04-19 21:28:56 +0200

    Empty project

---
+++ app/src/main/java/com/rickkorkmaz/androidmediaplayer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rickkorkmaz.androidmediaplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
