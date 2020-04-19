efe042c0b9b7acfe41c46efbc426a06f26657b7e
Rick <rickkorkmaz@gmail.com>
2020-04-19 22:03:45 +0200

    Plays and pauses audio resource

--- app/src/main/java/com/rickkorkmaz/androidmediaplayer/MainActivity.java
+++ app/src/main/java/com/rickkorkmaz/androidmediaplayer/MainActivity.java
@@ -2,13 +2,42 @@ package com.rickkorkmaz.androidmediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: Init mMediaPlayer
+        mMediaPlayer = MediaPlayer.create(this, R.raw.bensound_buddy);
+
+        setupButtons();
+    }
+
+    private void setupButtons() {
+        Button playButton = findViewById(R.id.play_button);
+        Button pauseButton = findViewById(R.id.pause_button);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMediaPlayer.start();
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMediaPlayer.pause();
+            }
+        });
     }
 }
